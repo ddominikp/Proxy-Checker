e34edd63511b262c93bec268231b561465fc6813
Dominik P <ddominikp@o2.pl>
2011-09-13 16:47:01 +0200

    added code documentation

--- main.cpp
+++ main.cpp
@@ -15,11 +15,42 @@ USAGE: ./ProxyChecker [ip1:port1] [ip2:port2] [ip3:port3] [ipN:portN]
 
 using namespace std;
 
-string ip, buffer, tmpProxyWorks, filename, proxytype;
-stringstream ipPort;
-vector<string> proxyTab;
-int i=0, proxiesOk = 0, proxiesFail = 0, port;
+string ip; /**< string that contains proxy ip address*/
+string buffer; /**< string used by writer function to save cURL*/
 
+//! contains proxy status
+/*!
+@return "ok" (proxy is working)
+@return "fail" (proxy isn't working)
+@return curl_strerror() (returns error description)
+@return "chfail" (cURL handle failed)
+*/
+string tmpProxyWorks;
+
+string filename; /**< string containing filename (later used to open/save proxy list file)*/
+
+//! contains proxy type
+/*!
+@return "http" (CURLPROXY_HTTP)
+@return "socks4" (CURLPROXY_SOCKS4)
+@return "socks4a" (CURLPROXY_SOCKS4A)
+@return "socks5" (CURLPROXY_SOCKS5)
+*/
+string proxytype;
+stringstream ipPort; /**< stringstream concatenating proxytype, ip, port*/
+vector<string> proxyTab; /**< vector containing proxy list*/
+int i=0; /**< integer used in various loops*/
+int proxiesOk = 0; /**< integer used for statistics (tells how many proxies are working)*/
+int proxiesFail = 0; /**< integer used for statistics (tells how many proxies aren't working)*/
+int port; /**< integer containing proxy port*/
+
+/**
+* This function gets called by libcurl as soon as there is data received that needs to be saved.
+* The size of the data pointed to by char *data is size multiplied with size_t nmemb, it will not be zero terminated.
+* Return the number of bytes actually taken care of.
+* If that amount differs from the amount passed to your function, it'll signal an error to the library.
+* This will abort the transfer and return CURLE_WRITE_ERROR.
+*/
 static int writer(char *data, size_t size, size_t nmemb, std::string *buffer){
     unsigned int result = 0;
     if (buffer != NULL){
@@ -28,6 +59,13 @@ static int writer(char *data, size_t size, size_t nmemb, std::string *buffer){
     }
   return result;
 }
+/**
+* function used to determine if proxy passed by arguments works
+* @param ip contains proxy ip (eg. 192.168.1.1)
+* @param port contains proxy port (eg. 8080)
+* @param proxytype it can contain one of 4 proxy types (http, socks4, socks4a, socks5), by default it contains http
+* @return proxy status ("ok", "fail", "chfail" [cURL handle initialization failed], "badformat", descriptive error returned by curl_strerror(res))
+*/
 string proxyWorks(string ip, int port, string proxytype="http"){
     CURL *ch;
     CURLcode res;
@@ -65,20 +103,20 @@ string proxyWorks(string ip, int port, string proxytype="http"){
 int main(int argc, char **argv)
 {
     if(argc>1){
-        int pTabIndex=0;
+        int pTabIndex=0; /**< proxyTab Index value*/
         if((strcmp(argv[1], "-f")==0 || strcmp(argv[1], "--filename")==0) && argv[2]){
             filename = argv[2];
 
-            ifstream fp;
+            ifstream fp; /**< file pointer used to read proxy list*/
             fp.open(filename.c_str());
             if(fp.is_open()){
-                string line;
+                string line; /**< string containing one line from proxy list*/
 
                 while(getline(fp, line)){
-                    char *dup=new char[line.length()+1];
+                    char *dup=new char[line.length()+1]; /**< char pointer containing line in char* format*/
                     strcpy(dup, line.c_str());
-                    char *pch=strchr(dup, ':');
-                    int n=0;
+                    char *pch=strchr(dup, ':'); /**< char pointer containing ':' position and later tokens divided by ":"*/
+                    int n=0; /**< integer used in while loop, it contains number of ':' in @see line variable*/
                     while(pch!=NULL){
                         pch = strchr(pch+1, ':');
                         n++;
@@ -125,8 +163,8 @@ int main(int argc, char **argv)
             }
         } else {
             for(i=1; i<argc; i++){
-                char *pch=strchr(argv[i], ':');
-                int n=0;
+                char *pch=strchr(argv[i], ':'); /**< char pointer containing ':' position and later tokens divided by ":"*/
+                int n=0; /**< integer used in while loop, it contains number of ':' in @see line variable*/
                 while(pch!=NULL){
                     pch = strchr(pch+1, ':');
                     n++;
@@ -172,11 +210,11 @@ int main(int argc, char **argv)
         cout <<"Proxies FAILED:\t"<<proxiesFail<<endl;
 
         if(proxiesOk>0){
-            char choice;
+            char choice; /**< char container used for user decision ('Y' == yes, 'N' == no)*/
             cout <<endl<<"Save proxies to file? [(Y)es or (N)o] ";
             if(cin >> choice){
                 if(choice=='Y' || choice == 'y'){
-                    ofstream fp;
+                    ofstream fp; /**< file pointer used to save proxyTab to file*/
                     cout <<"Path (filename): ";
                     cin >> filename;
                     cout <<endl;
